beb7bf7984bdaecaf53618b7484fc848985723b8
loreleymilazzo <clm12343@gmail.com>
2022-10-09 21:35:11 -0300

    back-end

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,128 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require ('axios');
+const { YOUR_API_KEY } = process.env;
 
 
+const { Dog ,  Temperament }= require("../db");
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+ const getApiInfo = async () => {
+    const apiUrl = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`) 
+    const apiInfo = await apiUrl.data.map(el =>{
+        return {
+            id: el.id,
+            image: el.image.url,
+            name: el.name,
+            temperament: el.temperament,
+            weight: el.weight.imperial,
+            height: el.height.imperial,
+            life_span: el.life_span,
+        }
+    })
+    return apiInfo
+
+}
+
+const getDbInfo = async () => {
+    return await Dog.findAll({
+      include: {
+        model: Temperament,
+        attributes: ["name"],
+        through: {
+    
+          attributes: [],
+        },
+      },
+    });
+  };
+
+ 
+ const getAllDogs  = async () => {
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    const infoTotal = apiInfo.concat(dbInfo);
+    return infoTotal;
+  };
+
+  router.get("/dogs", async (req, res) => {
+    const name = req.query.name 
+    let dogsTotal = await getAllDogs();
+    if(name){
+        let dogsName = await dogsTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase())) 
+        dogsName.length ?
+        res.status(200).send(dogsName) :
+        res.status(404).send( "No está el perrito :C");
+    } else{
+        res.status(200).send(dogsTotal)
+    
+    }
+  })
+
+  router.get("/temperaments", async (req, res) => {
+    const temperamentsApi = await axios.get (`https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`)
+    const temperamentsBd = temperamentsApi.data.map((e) => e.temperament)//muchos arrelos
+   .toString()//Devuelve una cadena de caracteres (texto)
+   .trim()// eliminar espacios en blanco y tablulaciones
+   .split(/\s*,\s*/);//Esto imprime dos líneas; la primera línea imprime la cadena original, y la segunda línea imprime el array resultante.
+   
+   const filtrado = temperamentsBd.filter(e => e);
+   const filtradoEach =[... new Set (filtrado)];
+   filtradoEach.forEach(el =>{
+      Temperament.findOrCreate({// se fija si esta y si no esta lo crea 
+         where: {name: el},
+      })
+   })
+   const allTemperaments = await Temperament.findAll();
+   res.json(allTemperaments);
+
+
+})
+
+router.post("/dogs", async (req, res) => {
+    const {
+      name,
+      height,
+      weight,
+      life_span,
+      temperament,
+      image,
+    } = req.body;
+  
+    const dogCreated = await Dog.create({
+      name:name,
+      height: height,
+      weight: weight,
+      life_span: life_span,
+      image: image,
+      createdInDb
+      
+    });
+
+    let temperamentDb = await Temperament.findAll({
+        where: {name : temperament  }
+    })
+    dogCreated.addTemperament(temperamentDb)
+
+    
+      res.send("Creaste una nueva raza");
+   
+  });
+
+  router.get("/dogs/:id", async (req, res) => {
+    const id = req.params.id;
+    const razas = await getAllDogs();
+    if (id){
+      const filtrados = await razas.filter((e) => e.id == id);
+      filtrados.length
+        ? res.status(200).json(filtrados)
+        : res.status(404).send("Raza no encontrada por ID");
+    }
+  });
+  
 
 
 module.exports = router;
